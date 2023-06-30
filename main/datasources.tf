2eafc1b567fb0516267a03251bcc6f1a50de23d4
Keith Lustria <keith.lustria@oracle.com>
2023-06-29 22:11:03 -0700

    Remove oke from main datasources

--- main/datasources.tf
+++ main/datasources.tf
@@ -22,38 +22,3 @@ data "oci_objectstorage_namespace" "object_storage_namespace" {
   #Optional
   compartment_id = var.compartment_ocid
 }
-
-# Used By OKE
-
-# Gets kubeconfig
-data "oci_containerengine_cluster_kube_config" "oke_cluster_kube_config" {
-  # cluster_id = var.create_new_oke_cluster ? module.oci-oke[0].cluster.id : var.existent_oke_cluster_id
-  cluster_id = var.create_new_oke_cluster ? oci_containerengine_cluster.oci_oke_cluster.id : var.existent_oke_cluster_id
-}
-
-# Gets a list of supported images based on the shape, operating_system and operating_system_version provided
-data "oci_core_images" "node_pool_images" {
-  compartment_id           = var.compartment_ocid
-  operating_system         = var.node_pool_image_operating_system
-  operating_system_version = var.node_pool_image_operating_system_version
-  shape                    = var.node_pool_shape
-  sort_by                  = "TIMECREATED"
-  sort_order               = "DESC"
-}
-
-data "oci_containerengine_cluster_option" "oke" {
-  cluster_option_id = "all"
-}
-data "oci_containerengine_node_pool_option" "oke" {
-  node_pool_option_id = "all"
-  compartment_id      = var.compartment_ocid
-}
-
-# OCI Services - Available Services
-data "oci_core_services" "all_services" {
-  filter {
-    name   = "name"
-    values = ["All .* Services In Oracle Services Network"]
-    regex  = true
-  }
-}
