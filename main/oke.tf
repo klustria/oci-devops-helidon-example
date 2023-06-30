d50eb8a219b61469a9f3576095a49e225cbe5cf4
Keith Lustria <keith.lustria@oracle.com>
2023-06-29 22:18:44 -0700

    Pass compartment id to oke module

--- main/oke.tf
+++ main/oke.tf
@@ -2,7 +2,8 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 module "oci-oke" {
-  count                                                                       = var.create_new_oke_cluster ? 1 : 0
-  source                                                                      = "./oke" # "github.com/oracle-quickstart/oci-oke"
-  availability_domains                                                        = data.oci_identity_availability_domains.ads.availability_domains
+  count                = var.create_new_oke_cluster ? 1 : 0
+  source               = "./oke" # "github.com/oracle-quickstart/oci-oke"
+  availability_domains = data.oci_identity_availability_domains.ads.availability_domains
+  compartment_ocid     = var.compartment_ocid
 }
