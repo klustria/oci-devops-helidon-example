5d56a34e68e0da31e0fe5630b27db628908769d7
Keith Lustria <keith.lustria@oracle.com>
2023-07-05 09:38:53 -0700

    Remove artifact that has already been moved to instance/devops.tf

--- main/devops.tf
+++ main/devops.tf
@@ -26,41 +26,6 @@ resource "oci_devops_repository" "devops_repo" {
   default_branch  = "main"
 }
 
-#### Start of deploy artifacts code which will be used by the Upload Artifact Stage and the Deploy Instance Group Stage #####
-
-# Create deployment spec artifact to use. The deployment spec will be renamed to "deployment_manifest.yaml"
-# when uploaded to Artifact Repository.
-resource "oci_devops_deploy_artifact" "devops_deployment_spec_artifact" {
-  argument_substitution_mode = "SUBSTITUTE_PLACEHOLDERS"
-  deploy_artifact_type       = "DEPLOYMENT_SPEC"
-  project_id                 = oci_devops_project.devops_project.id
-  display_name               = "devops-deployment-spec-artifact${local.resource_name_suffix}"
-  deploy_artifact_source {
-    deploy_artifact_path        = "deployment_manifest.yaml"
-    deploy_artifact_source_type = "GENERIC_ARTIFACT"
-    deploy_artifact_version     = "$${BUILDRUN_HASH}"
-    repository_id               = oci_artifacts_repository.artifact_repo.id
-  }
-}
-
-# Create application artifact to use. The generic file which will correspond to the Helidon App
-# will be renamed to "helidon-oci-mp.tgz" when uploaded to Artifact Repository.
-resource "oci_devops_deploy_artifact" "devops_application_artifact" {
-  argument_substitution_mode = "SUBSTITUTE_PLACEHOLDERS"
-  deploy_artifact_type       = "GENERIC_FILE"
-  project_id                 = oci_devops_project.devops_project.id
-  display_name               = "devops-application-artifact${local.resource_name_suffix}"
-  deploy_artifact_source {
-    deploy_artifact_path        = "helidon-oci-mp.tgz"
-    deploy_artifact_source_type = "GENERIC_ARTIFACT"
-    deploy_artifact_version     = "$${BUILDRUN_HASH}"
-    repository_id               = oci_artifacts_repository.artifact_repo.id
-  }
-}
-
-#### End of deploy artifacts code #####
-
-
 # Create log group that will serve as the logical container for the devops log
 resource "oci_logging_log_group" "devops_log_group" {
   compartment_id = var.compartment_ocid
