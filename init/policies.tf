a92ea3a53387699951cffba71ea114997f3b53ef
Keith Lustria <keith.lustria@oracle.com>
2023-07-10 23:53:34 -0700

    Add policy for cluster family devops management

--- init/policies.tf
+++ init/policies.tf
@@ -8,6 +8,7 @@ resource "oci_identity_policy" "devops_policy" {
   compartment_id = var.tenancy_ocid
 
   statements = [
+    "Allow dynamic-group ${oci_identity_dynamic_group.devops_dynamic_group.name} to manage cluster-family in compartment ${oci_identity_compartment.devops_demo_compartment.name}",
     "Allow dynamic-group ${oci_identity_dynamic_group.devops_dynamic_group.name} to manage devops-family in compartment ${oci_identity_compartment.devops_demo_compartment.name}",
     "Allow dynamic-group ${oci_identity_dynamic_group.devops_dynamic_group.name} to manage repos in compartment ${oci_identity_compartment.devops_demo_compartment.name}",
     "Allow dynamic-group ${oci_identity_dynamic_group.devops_dynamic_group.name} to manage all-artifacts in compartment ${oci_identity_compartment.devops_demo_compartment.name}",
