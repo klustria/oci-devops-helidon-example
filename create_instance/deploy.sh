77c6560f5753a9151380ed9d9d4447c5094c34f2
Keith Lustria <keith.lustria@oracle.com>
2025-04-23 01:17:00 -0700

    fix run command

--- create_instance/deploy.sh
+++ create_instance/deploy.sh
@@ -56,7 +56,7 @@ ssh -i private.key opc@"${PUBLIC_IP}" << 'EOF'
     curl -O https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
     tar xvzf jdk-21_linux-x64_bin.tar.gz
     export PATH=~/jdk-21.0.7/bin:$PATH
-    nohup java -jar target/oci-mp-server.jar &> ci-mp-server.log &
+    nohup java -jar oci-mp-server.jar &> oci-mp-server.log &
 EOF
 
 # delete private.key and application zip
