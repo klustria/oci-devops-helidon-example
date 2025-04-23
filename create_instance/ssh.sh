1418e7e34ffbf167a276d6d040e2e29dcfb8d4f1
Keith Lustria <keith.lustria@oracle.com>
2025-04-23 01:34:55 -0700

    create ssh script

---
+++ create_instance/ssh.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Copyright (c) 2025 Oracle and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+SCRIPT_DIR=$(dirname $0)
+
+# Generate private key file that will be use to ssh or scp to the instance
+"${SCRIPT_DIR}"/get.sh create_ssh_private_key
+# Get instance public IP
+PUBLIC_IP=$("${SCRIPT_DIR}"/get.sh public_ip)
+# ssh to the instance
+ssh -o StrictHostKeyChecking=accept-new -i private.key opc@"${PUBLIC_IP}"
+# delete private.key and application zip
+rm private.key
